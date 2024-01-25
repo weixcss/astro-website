5e50261a98e97f01fb1cae8d9d10868de5f271b6
weixcss <weixcss@gmail.com>
2024-01-25 13:23:38 -0500

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Astro Demonstration';
+export const SITE_DESCRIPTION = 'Created by Wei!';
